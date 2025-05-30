e95845f44a92e60378a7b2882192a73c054d3678
slayer <meerutuniversity996@gmail.com>
2025-05-30 10:15:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+
+API_ID = "4014305"
+
+API_HASH = "b0cb9e17b2b8bcde3be7161c8bfa6013"
+
+# Telegram bot token
+TELEGRAM_TOKEN = ""
+
+# Your telegram user ID (owner)
+OWNER_ID = 6018803920
+
+SUDO_ID = [6018803920]
+
+MONGO_URI = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
+
+DB_NAME = "Nullcrow"
+
+LOGGER = True
+
+BOT_NAME = "Edit Guardian"
+
+SUPPORT_ID = (-1002392274240)
